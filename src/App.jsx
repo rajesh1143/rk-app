f457b00aa72afd2b247b836f64021b89ec3cb0cb
RajeshKumar Perumal <rperumal270@everestdx.com>
2024-12-03 18:40:56 +0530

    added mantine

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import "@mantine/core/styles.css";
 import "./App.css";
+import { MantineProvider, Text } from "@mantine/core";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 style={{ color: "red" }}>My App</h1>
-    </>
+    <MantineProvider>
+      <Text c={red}>My App</Text>
+    </MantineProvider>
   );
 }
 

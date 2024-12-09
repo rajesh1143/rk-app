1f1b2a5f367c36862ed0bc27b82e3d1468667c43
RajeshKumar Perumal <rperumal270@everestdx.com>
2024-12-09 18:44:23 +0530

    updated routing config and app setup

---
+++ src/app/router.jsx
@@ -0,0 +1,35 @@
+import { createHashRouter, Link, Outlet, RouterProvider } from "react-router-dom";
+
+const appRouter = createHashRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contactus">Contact Us</Link>
+        <br/>
+        <Outlet/>
+      </>
+    ),
+    children:[
+        {
+            path:"/",
+            element:"Home"
+        },
+        {
+            path:"/about",
+            element:"About"
+        },
+        {
+            path:"/contactus",
+            element:"contact Us"
+        }
+    ],
+    errorElement:"404: PageNotFound"
+  },
+]);
+
+export const AppRouter = () => {
+    return <RouterProvider router={appRouter}/>
+}
